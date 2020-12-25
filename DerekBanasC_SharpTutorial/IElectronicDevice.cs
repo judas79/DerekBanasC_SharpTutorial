aaee3245f29ada5263dc32566e2992151178cd1d
Rodman Underhill <robotrods@gmail.com>
2020-12-25 17:05:58 -0500

    update tutorial 10 published

---
+++ DerekBanasC_SharpTutorial/IElectronicDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekBanasC_SharpTutorial
+{
+    // With interfaces you can create very
+    // flexible systems. Here I'll model
+    // generic electronic devices like 
+    // TVs and Radios and remotes that 
+    // control them and the buttons on the
+    // remotes. 
+    interface IElectronicDevice
+    {
+        // We want each device to have 
+        // these capabilities, 
+        // all the devices share these capabilities
+        void On();
+        void Off();
+        void VolumeUp();
+        void VolumeDown();
+
+    }
+}
---
+++ DerekBanasC_SharpTutorial/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekBanasC_SharpTutorial
+{
+    class Vehicle : IDrivable
+    {
+        // specific to Vehicle class, properties
+        public string Brand { get; set; }
+
+        // constructor
+        public Vehicle(string brand = "No Brand",
+            // number of wheels not doe it have wheels
+            int wheels = 0,
+            double speed = 0)
+        {
+            Brand = brand;
+            Wheels = wheels;
+            Speed = speed;
+        }
+
+        // Properties and methods from
+        // the interface
+        public double Speed { get; set; }
+
+        public int Wheels { get; set; }
+
+        public void Move()
+        {
+            Console.WriteLine($"The {Brand} Moves Forward at {Speed} MPH");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"The {Brand} Stops");
+            Speed = 0;
+        }
+    }
+}
